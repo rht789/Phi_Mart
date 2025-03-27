1f77a7d84a46ca2be9e2174a8b019fa4f21e8b1b
Md. Rezaul Karim <rezaulrahaat@gmail.com>
2025-03-27 19:49:05 +0600

    20.1

--- product/views.py
+++ product/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 # Create your views here.
+@api_view()
+def view_products(request):
+    return Response({'message':'okay'})
+
+@api_view()
+def view_categories(request):
+    return Response({'message':'okay'})
