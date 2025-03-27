4072312c6f33cd30e7b160ec3bd74238652d0c39
Md. Rezaul Karim <rezaulrahaat@gmail.com>
2025-03-28 03:00:48 +0600

    20.2

--- product/views.py
+++ product/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from product.models import Product,Category
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
 # Create your views here.
 @api_view()
-def view_products(request):
-    return Response({'message':'okay'})
+def view_specific_products(request,id):
+    product=get_object_or_404(Product, pk=id)
+    product_dict={"id":product.id, "name":product.name, "price":product.price}
+    return Response(product_dict)
 
 @api_view()
 def view_categories(request):
